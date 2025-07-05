806a2332e55f89a976e19c33cc1e243de4eae1c2
Roman Trippel <contact@romantrippel.com>
2025-07-05 17:58:45 +0200

    feat(cors): allow frontend origin to access API

---
+++ src/main/java/com/romantrippel/dictionary/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.romantrippel.dictionary.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://dictionary.romantrippel.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+}
